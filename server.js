4aad31b383efca3d9b7d806243344b344043b572
sergax <sergey.aksenchenko@gmail.com>
2022-09-18 23:56:18 +0300

    added deploy configs

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/app-course-client'));
+app.use(express.static(__dirname + '/dist/course-client'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/app-course-client/index.html'));});
+    '/dist/course-client/index.html'));});
 app.listen(process.env.PORT || 8080);
 
