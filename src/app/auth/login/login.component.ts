10e2e2df3feb98e94dae90d3a5ea072ed6665459
sergax <sergey.aksenchenko@gmail.com>
2022-10-13 20:20:47 +0300

    fixed typo

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -4,43 +4,43 @@ import {AuthService} from "../auth.service";
 import {Router} from "@angular/router";
 
 @Component({
-  selector: 'auth-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'auth-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  isLoading = false;
-  isAuthorized: boolean = true;
+    isLoading = false;
+    isAuthorized = true;
 
-  exform!: FormGroup;
+    exform!: FormGroup;
 
-  constructor(public authService: AuthService,
-              private router: Router) {
-  }
+    constructor(public authService: AuthService,
+                private router: Router) {
+    }
 
-  ngOnInit() {
-    this.exform = new FormGroup({
-      'email': new FormControl(null, [Validators.required, Validators.email]),
-      'password': new FormControl(null, [Validators.required, Validators.minLength(8)])
-    });
-  }
+    ngOnInit() {
+        this.exform = new FormGroup({
+            'email': new FormControl(null, [Validators.required, Validators.email]),
+            'password': new FormControl(null, [Validators.required, Validators.minLength(8)])
+        });
+    }
 
-  onLogin() {
-    this.isLoading = true;
-    this.authService.login(this.exform.value.email, this.exform.value.password);
-    this.authService.loginSubject.subscribe(value => {
-      this.isAuthorized = value;
-      if (value) {
-        this.router.navigate(['courses']);
-      }
-    })
-  }
+    onLogin() {
+        this.isLoading = true;
+        this.authService.login(this.exform.value.email, this.exform.value.password);
+        this.authService.loginSubject.subscribe(value => {
+            this.isAuthorized = value;
+            if (value) {
+                this.router.navigate(['courses']);
+            }
+        })
+    }
 
-  get email() {
-    return this.exform.get('email');
-  }
+    get email() {
+        return this.exform.get('email');
+    }
 
-  get password() {
-    return this.exform.get('password');
-  }
+    get password() {
+        return this.exform.get('password');
+    }
 }
