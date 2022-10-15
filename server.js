c241935c0e40fe77daade7fa3087968acfd305de
sergax <sergey.aksenchenko@gmail.com>
2022-10-15 18:41:59 +0300

    fixed incorrect validation input password

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/course-client'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/course-client/index.html'));});
-app.listen(process.env.PORT || 8080);
-
